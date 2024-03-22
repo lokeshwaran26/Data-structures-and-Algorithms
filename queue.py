bc8f204fff90b29e44cd520658f996973dfcfdc5
lokesh <lokesh95664@gmail.com>
2024-03-22 18:18:56 +0530

    ....

---
+++ queue.py
@@ -0,0 +1,18 @@
+from collections import deque
+
+class Queue:
+
+    def __init__(self):
+        self.buffer = deque()
+
+    def enqueue(self, val):
+        self.buffer.appendleft(val)
+
+    def dequeue(self):
+        return self.buffer.pop()
+
+    def is_empty(self):
+        return len(self.buffer) == 0
+    
+    def size(self):
+        return len(self.buffer)
